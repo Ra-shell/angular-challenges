c03194b8a01df6796a86cff1f0f9194dcb27894a
Rachel Wedewer <Wedewer.Rachel@gmail.com>
2025-03-09 17:36:40 -0500

    feat(challenge53): refactor to use derived states

--- apps/signal/53-big-signal-performance/src/app/user.service.ts
+++ apps/signal/53-big-signal-performance/src/app/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class UserStore {
@@ -13,4 +13,18 @@ export class UserStore {
     title: '',
     salary: 0,
   });
+
+  public readonly userName = computed(() => this.user().name);
+  public readonly userAddress = computed(() => this.user().address, {
+    equal: (a, b) => {
+      return (
+        (a === undefined && b === undefined) ||
+        (a === null && b === null) ||
+        (a.city === b.city && a.street === b.street && a.zipCode === b.zipCode)
+      );
+    },
+  });
+  public readonly userNote = computed(() => this.user().note);
+  public readonly usertitle = computed(() => this.user().title);
+  public readonly userSalary = computed(() => this.user().salary);
 }
